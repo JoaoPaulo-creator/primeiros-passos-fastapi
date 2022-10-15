537bbdc66ac984695f4a6d118467e5795d21158a
JoaoPaulo-creator <joaosarto@gmail.com>
2022-10-15 14:58:41 -0300

    Ajustes feitos para permitir acesso de outro server + ajuste para poder ser consumido pelo front

--- tests/teste_requisicao.py
+++ tests/teste_requisicao.py
@@ -5,3 +5,10 @@ def test_fazer_requisicao():
     requisicao = get('http://127.0.0.1:8000/item/foo?obrigatorio=yes&skip=0&limit=2')
     requisicao_to_json = requisicao.json()
     print(requisicao_to_json)
+
+
+def test_iterar_lista_dicionario():
+    lista_dict = [{'chave': 'valor'}, {'chave2': 1}]
+
+    for i in lista_dict:
+        print(i)
